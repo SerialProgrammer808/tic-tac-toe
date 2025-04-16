50f14c323b9b58aa37311becb266137eda9f7b8d
Noah Asano <pili.asano@gmail.com>
2025-04-16 08:53:16 -1000

    Add playGame function and game/player turn validation.
    Keeping with the theme of modular code, this project requires that a playGame function be made to keep track of the flow of the game. Another requirement that became apparent along the way was validating if the game was won and if the players turn was valid. This commit will add a function to keep track of the game andvalidation for players/game condition. This makes the game fully functional from the console

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //Gameboard Object
 const gameBoard = (function gameBoard(){
     let board = ["*", "*", "*", "*", "*", "*", "*", "*", "*"];
-    return {board};
+    let isWinner = (board) => {
+        //check horizontal combinations
+        for (let counter = 0; counter < 6; counter += 3) {
+            if (board[counter] !== "*" && board[counter] === board[counter+1] && board[counter+1] === board[counter+2]) {
+                alert(`${board[counter]} wins!`)
+                return true;
+            }
+        }
+        //check vertical combinations
+        for (let counter = 0; counter < 3; counter++) {
+            if (board[counter] !== "*" && board[counter] === board[counter+3] && board[counter+3] === board[counter+6]) {
+                return true;
+            }
+        }
+
+        //check crossed combinations
+        if ((board[0] !== "*" &&board[0] === board[4] && board[4] === board[8]) || 
+        (board[2] !== "*" && board[2] === board[4] && board[4] === board[6])) {
+            return true;
+        }
+    }
+
+    return {board, isWinner};
 })();
 
 //Player Object
@@ -9,27 +31,38 @@ function player(letter) {
     if (letter !== "X" && letter !== "O") {
         return false;
     }
+
     const marker = letter;
 
-
-    function myTurn(number) {
-        if ((number >= 1 && number <= 9) && (gameBoard.board[number-1] === "*")) {
-            gameBoard.board[number-1] = marker;
+    function myTurn() {
+        while (true) {
+            let number = parseInt(prompt(`Player ${marker}, pick a spot (1-9):`), 10);
+            if ((number >= 1 && number <= 9) && (gameBoard.board[number-1] === "*")) {
+                gameBoard.board[number-1] = marker;
+                break;
+            }
         }
+        console.log(gameBoard.board);
     }
     return {myTurn, marker};
 }
 
 //Flow of Game Object
-
+function playGame() {
+    let counter = 0;
+    while (counter < 9 && !gameBoard.isWinner(gameBoard.board)) {
+        if (counter % 2 === 0) {
+            player1.myTurn();
+        }
+        else {
+            player2.myTurn();
+        }
+        counter++;
+    }
+}
 
 //Global code
-console.log(gameBoard.board);
-
 let player1 = player("X");
 let player2 = player("O");
-console.log(player1);
-player1.myTurn(1);
-player2.myTurn(2);
 
-console.log(gameBoard.board);
+playGame();
